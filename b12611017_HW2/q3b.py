dc6bd4edd97c07812b5993b4e6567a2829393f39
lego <gdteyuj123@gmail.com>
2024-10-01 15:51:44 +0800

    add ( ) at if

--- b12611017_HW2/q3b.py
+++ b12611017_HW2/q3b.py
@@ -4,7 +4,7 @@ unique_numbers = list(set(numbers))
 
 for i in range(len(unique_numbers)):
     for j in range(0, len(unique_numbers) - 1 - i):
-        if unique_numbers[j] > unique_numbers[j + 1]:
+        if(unique_numbers[j] > unique_numbers[j + 1]):
             unique_numbers[j], unique_numbers[j + 1] = unique_numbers[j + 1], unique_numbers[j]
 
 print(",".join(str(x) for x in unique_numbers))
