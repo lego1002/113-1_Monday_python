ea1f31480486612fa8d8897a52deaa54b1354d95
lego <gdteyuj123@gmail.com>
2024-10-01 15:51:03 +0800

    add bubble sort to q4b.py

--- b12611017_HW2/q4b.py
+++ b12611017_HW2/q4b.py
@@ -1,12 +1,19 @@
 def find_common_elements(list1, list2):
     common_elements = set(list1) & set(list2)
     if(common_elements):
-        return(",".join(common_elements))
+        return list(common_elements)
     else:
-        return("N/A")
+        return []
 
 list1 = input().split(",")
 list2 = input().split(",")
-
 result = find_common_elements(list1, list2)
-print(result)
+
+if(result):
+    for i in range(len(result)):
+        for j in range(0, len(result) - 1 - i):
+            if(result[j] > result[j + 1]):
+                result[j], result[j + 1] = result[j + 1], result[j]
+    print(",".join(result))
+else:
+    print("N/A")
